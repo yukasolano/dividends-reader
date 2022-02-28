17cdc33b8e4b4aea2a6f98e2777aa0514ea0ae71
Yuka Solano <yukakyushima@gmail.com>
2022-02-28 19:34:27 -0300

    Reading dividends from nuinvest

--- src/main/java/br/com/nuinvest/DividendsReader.java
+++ src/main/java/br/com/nuinvest/DividendsReader.java
@@ -1,7 +1,81 @@
 package br.com.nuinvest;
 
+import br.com.nuinvest.model.Transaction;
+
+import java.io.*;
+import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
+
 public class DividendsReader {
-    public static void main(String[] args) {
-        System.out.println("Hello word");
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("file-test.csv");
+        FileInputStream inputStream = new FileInputStream(file);
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            String line;
+            int count = 0;
+            List<Transaction> transactions = new ArrayList<>();
+            List<String> remains = new ArrayList<>();
+            while ((line = br.readLine()) != null) {
+
+                if (count > 1) {
+                    String[] split = line.split(";");
+                    String code = split[5];
+
+                    if (code.equals("200")) {
+                        LocalDate date = convertToDate(split[1]);
+                        BigDecimal value = convertToValue(split[3]);
+                        Integer quantity = convertToInteger(split[2].substring(14, 33));
+                        String stock = split[2].substring(39, 45);
+                        Transaction transaction = new Transaction(date, stock, quantity, value, code);
+                        transactions.add(transaction);
+                    } else {
+                        remains.add(line);
+                    }
+                }
+                count++;
+            }
+
+            Map<String, Set<Transaction>> byStock = transactions.stream()
+                    .collect(groupingBy(Transaction::getStock, TreeMap::new, Collectors.toSet()));
+
+
+            byStock.forEach((k, v) -> {
+                BigDecimal sum = v.stream().map(Transaction::getTotal)
+                        .reduce(BigDecimal.ZERO, BigDecimal::add);
+                DecimalFormat df = new DecimalFormat("####.00");
+                System.out.printf("%s\t %s%n", k, df.format(sum));
+            });
+        } catch (ParseException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static LocalDate convertToDate(String dateString) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return LocalDate.parse(dateString, formatter);
+    }
+
+    private static BigDecimal convertToValue(String valueString) throws ParseException {
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols();
+        symbols.setGroupingSeparator('.');
+        symbols.setDecimalSeparator(',');
+        String pattern = "#,##0.0#";
+        DecimalFormat decimalFormat = new DecimalFormat(pattern, symbols);
+        decimalFormat.setParseBigDecimal(true);
+        return (BigDecimal) decimalFormat.parse(valueString);
+    }
+
+    private static Integer convertToInteger(String valueString) throws ParseException {
+        return Integer.valueOf(valueString.trim().replace(".", ""));
     }
 }
---
+++ src/main/java/br/com/nuinvest/model/Transaction.java
@@ -0,0 +1,46 @@
+package br.com.nuinvest.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Transaction {
+    private final LocalDate date;
+    private final String stock;
+    private final Integer quantity;
+    private final BigDecimal total;
+    private String type;
+    private final String code;
+
+    public Transaction(LocalDate date, String stock, Integer quantity, BigDecimal total, String code) {
+        this.date = date;
+        this.stock = stock;
+        this.quantity = quantity;
+        this.total = total;
+        this.code = code;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String getStock() {
+        return stock;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+}
