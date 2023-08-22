5a66ca5ccf2ea1f007f035a4ea365f33edd73863
Yuka Solano <yukakyushima@gmail.com>
2023-08-22 10:51:48 +0100

    Create nuinvest case

--- src/main/java/br/com/dividends/NuInvestDividendsReader.java
+++ src/main/java/br/com/dividends/NuInvestDividendsReader.java
@@ -2,6 +2,7 @@ package br.com.dividends;
 
 import br.com.dividends.adapter.NuInvestLineToTransaction;
 import br.com.dividends.model.Report;
+import br.com.dividends.model.ReportMonthly;
 import br.com.dividends.model.Transaction;
 import br.com.dividends.reader.TransactionsReader;
 
@@ -11,8 +12,8 @@ import java.util.*;
 
 public class NuInvestDividendsReader {
     public static void main(String[] args) {
-        List<Transaction> transactions = TransactionsReader.read("Extrato_2023-01-01.csv", new NuInvestLineToTransaction());
+        List<Transaction> transactions = TransactionsReader.read("Extrato_2023-07-29.csv", new NuInvestLineToTransaction());
         Report.buildReport(transactions).print();
-        // ReportMonthly.buildReport(transactions).print();
+        //ReportMonthly.buildReport(transactions).print();
     }
 }
